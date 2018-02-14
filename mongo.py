26ce78974803c2f08dd8fe60a581871cf903bc5c
Stanley Lin <slin55055@gmail.com>
2018-02-14 14:50:17 -0500

    WIP

---
+++ mongo.py
@@ -0,0 +1,51 @@
+import pymongo
+
+connection = pymongo.MongoClient("homer.stuy.edu")
+
+db = connection.test
+
+col = db.restaurants
+
+print col.find({}).count()
+
+def print_cursor(cursor):
+	for d in cursor:
+		if d['name'] == "Nordic Delicacies":
+			print d['name']
+
+def find_borough(bor):
+	cursor = col.find({
+		"borough": bor
+	})
+	print_cursor(cursor)
+
+def find_zip(zipcode):
+	cursor = col.find({
+		"address.zipcode": zipcode
+	})
+	print_cursor(cursor)
+
+def find_zip_grade(zipcode, grade):
+	print_cursor(
+	col.find({
+		"address.zipcode": zipcode,
+		"grades.grade": grade
+	})
+	)
+
+def find_zip_score(zipcode, score):
+	print_cursor(
+	col.find({
+		"address.zipcode": zipcode,
+		"grades.score": {
+			"$lt": score
+		}
+	})
+	)
+
+#find_borough("Bronx")
+#find_zip("11209")
+#find_zip_grade("11209", "A")
+find_zip_score("11209", 11)
+
+connection.close()
