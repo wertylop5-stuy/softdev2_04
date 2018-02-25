a0e7555f1f1e4639659ce83eb9492bee6b05934f
Stanley Lin <slin55055@gmail.com>
2018-02-25 18:33:45 -0500

    finished

--- mongod.py
+++ mongod.py
@@ -1,4 +1,15 @@
+'''
+Current US Representatives: a list of all the state representatives currently serving
+in Congress.
+
+Download: https://www.govtrack.us/api/v2/role?current=true&role_type=representative&limit=438
+
+We went through every element in the list "objects" and inserted
+those into the database.
+'''
+
 import pymongo
+import json
 
 connection = pymongo.MongoClient("homer.stuy.edu")
 
@@ -9,7 +20,49 @@ col = db.data
 filename = "data.json"
 file = open(filename, "r")
 
-for x in file:
-    print x
-    break
+js = json.load(file)
+
+#Inserts the data into the db
+'''
+for x in js['objects']:
+    col.insert_one(x);
+'''
+
+def print_cursor(cursor):
+    for d in cursor:
+            print d
+
+def find_party(party):
+    cursor = col.find({
+        "party": party
+    })
+    print_cursor(cursor)
+
+def find_person(last, first):
+    cursor = col.find({
+        "person.lastname": last,
+        "person.firstname": first
+    })
+    print_cursor(cursor)
+
+def find_twitter(twitter_id):
+    cursor = col.find({
+        "person.twitterid": twitter_id
+    })
+    print_cursor(cursor)
+
+def find_state(state_code):
+    print_cursor(
+        col.find({
+            "state": state_code
+        })
+    )
+
+
+#find_party("Republican")
+#find_person("Aderholt", "Robert")
+#find_twitter("Robert_Aderholt")
+find_state("AL")
+
+connection.close()
 file.close()
